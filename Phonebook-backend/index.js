9c997ed383111bd36c14ce89e865da9ca391c0af
mohamed-aje <chochoaj93@gmail.com>
2022-03-15 11:18:27 +0200

    static build config

--- Phonebook-backend/index.js
+++ Phonebook-backend/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
-app.use(express.json());
+app.use(express.static("build"));
 let persons = [
   {
     id: 1,
