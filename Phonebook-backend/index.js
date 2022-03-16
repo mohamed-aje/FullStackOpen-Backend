241abe99d773c27ba61ecda783152c7e9b720ba1
mohamed-aje <chochoaj93@gmail.com>
2022-03-16 13:13:03 +0200

    3.18

--- Phonebook-backend/index.js
+++ Phonebook-backend/index.js
@@ -2,13 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-
+const bodyParser = require("body-parser"); // <=== this line
+app.use(bodyParser.json()); //<=== This line
 const Person = require("./models/person");
-
 app.use(express.static("build"));
 const cors = require("cors");
+const person = require("./models/person");
 app.use(cors());
 morgan.token("object", function (req) {
   return `${JSON.stringify(req.body)}`;
@@ -20,6 +19,18 @@ app.use(morgan(":method :url :status :response-time :req[header] :object"));
 //const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
 //return maxId + 1;
 //};
+app.get("/api/persons/:id", (req, res, next) => {
+  person
+    .findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => next(err));
+});
 //adding to
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -30,6 +41,12 @@ app.get("/api/persons", (request, response) => {
     response.json(persons);
   });
 });
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
 app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
@@ -49,17 +66,21 @@ app.post("/api/persons", (request, response, next) => {
     })
     .catch((error) => next(error));
 });
-{
-  /*app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((p) => p.id !== id);
-});*/
-}
+
+app.delete("/api/persons/:id", (req, res, next) => {
+  person
+    .findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((err) => next(err));
+});
+
 //info page
 app.get("/info", (request, response) => {
   const newdate = new Date();
   response.send(
-    `phone book has info for ${persons.length} people<br>${newdate}`
+    `phone book has info for ${person.length} people<br>${newdate}`
   );
 });
 //home page
@@ -67,15 +88,6 @@ app.get("/info", (request, response) => {
 //getting all persons
 
 //getting collection by id
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((p) => p.id === id);
-  if (person) {
-    res.json(person);
-  } else {
-    response.status(404).end();
-  }
-});
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
